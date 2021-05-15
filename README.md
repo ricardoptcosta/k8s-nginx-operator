66f0c0d2af1fedd91118fb6ea2a163043961d115
ric <ricardoptcosta@gmail.com>
2021-05-16 00:29:58 +0100

    create basic operator feature

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # k8s-nginx-operator
+
+Requirements:
+
+Minikube v1.22+
+Kubectl
+
+
+This Kubernetes operator creates, updates and deletes Nginx Deployments based on configmaps that are created, updated and deleted.
+
+In order to test have three terminal windows open:
+- Window 1 - run operator:
+  - $ bash operator.sh
+
+- Window 2 - create, delete or update configmaps:
+  - $ kubectl apply -f sample-configmap.yml
+  - $ kubectl delete -f sample-configmap.yml
+
+- Window 3 - check deployments being created, deleted or updated:
+  - $ kubectl get deployments
+
+
+To see the text written on the configmap displayed on the browser, do port-forwarding
---
+++ operator.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+kubectl get --watch --output-watch-events configmap -o=custom-columns=type:type,name:object.metadata.name --no-headers | \
+	while read next; do
+		NAME=$(echo $next | cut -d' ' -f2)
+                EVENT=$(echo $next | cut -d' ' -f1)
+	
+		case $EVENT in
+                  ADDED|MODIFIED)
+			  kubectl apply -f - << EOF
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: $NAME
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: $NAME
+  template:
+    metadata:
+      labels:
+        app: $NAME
+    spec:
+      containers:
+      - image: nginx
+        name: $NAME
+        ports:
+        - containerPort: 80
+        volumeMounts:
+        - { name: data, mountPath: /usr/share/nginx/html }
+      volumes:
+      - name: data
+        configMap:
+         name: $NAME
+EOF
+			   ;;
+					DELETED)
+                    kubectl delete deploy $NAME
+                    ;;
+          esac
+done
